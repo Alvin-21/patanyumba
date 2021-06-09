0837bb3b2b0f59a0223db263499487e5794ab408
Alvin Awuor <alvinawuor8@gmail.com>
2021-06-09 13:41:48 +0300

    Add cloudinary configs.

--- patanyumba/settings.py
+++ patanyumba/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gethouse',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +130,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+cloudinary.config(
+    cloud_name="alvo",
+    api_key="446484527969114",
+    api_secret="bhfiQsAGOjj-KphZPB7RogckT_o"
+)
