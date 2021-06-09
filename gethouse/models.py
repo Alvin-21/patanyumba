8275132dd960cfe00c19e117dcb18b04ad83eac2
Alvin Awuor <alvinawuor8@gmail.com>
2021-06-09 15:16:34 +0300

    Add model for amenities.

--- gethouse/models.py
+++ gethouse/models.py
@@ -3,3 +3,9 @@ from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class amenities(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
