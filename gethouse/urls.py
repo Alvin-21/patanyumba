ad1b536b2435fb7f2432a736b22fe2d348577af5
Alvin Awuor <alvinawuor8@gmail.com>
2021-06-10 18:35:18 +0300

    Add url for add new accom

--- gethouse/urls.py
+++ gethouse/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     re_path(r'^ajax/subscription/$', views.subscription, name='subscription'),
     re_path(r'^search/', views.search_results, name='search_results'),
     re_path(r'^accomodation/(\d+)', views.accomodation, name='accomodation'),
+    re_path(r'^new/accomodation$', views.new_accom, name='new_accomodation'),
 ]
