80b3745ac0ec076c4f62d623d65325d4e34f49a9
Alvin Awuor <alvinawuor8@gmail.com>
2021-06-10 11:14:21 +0300

    Add subscription form.

--- gethouse/forms.py
+++ gethouse/forms.py
@@ -5,3 +5,8 @@ class AccomodationForm(forms.ModelForm):
     class Meta:
         model = Accomodation
         exclude = ['user']
+
+
+class SubscriptionForm(forms.Form):
+    name = forms.CharField(label='First Name', max_length=30)
+    email = forms.EmailField(label='Email')
