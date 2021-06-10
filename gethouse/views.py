cf4109a762becf5367ad2e9412129269338b9491
Alvin Awuor <alvinawuor8@gmail.com>
2021-06-10 12:43:45 +0300

    Add method for subscribing.

--- gethouse/views.py
+++ gethouse/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.http import HttpResponse, Http404, HttpResponseRedirect, JsonResponse
 from .models import *
 from .forms import *
+from .email import send_welcome_email
 
 # Create your views here.
 
 def index(request):
     accom = Accomodation.objects.all()
-    return render(request, 'index.html', {"accoms": accom})
+    form = SubscriptionForm()
+    return render(request, 'index.html', {"accoms": accom, "form": form})
+
+def subscription(request):
+    name = request.POST.get('name')
+    email = request.POST.get('email')
+
+    recipient = SubscriptionRecipients(name=name, email=email)
+    recipient.save()
+    send_welcome_email(name, email)
+    data = {'success': 'You have been successfully added to mailing list'}
+    return JsonResponse(data)
 
 def accomodation(request, accom_id):
     accom = Accomodation.get_accom_by_id(accom_id)
