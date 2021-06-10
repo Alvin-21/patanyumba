22913cbab64b95b259cd757a176c35139a6b6f96
Alvin Awuor <alvinawuor8@gmail.com>
2021-06-10 16:25:30 +0300

    Register models to admin.

--- gethouse/admin.py
+++ gethouse/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class AccomodationAdmin(admin.ModelAdmin):
+    filter_horizontal = ('Amenities',)
+
+admin.site.register(Profile)
+admin.site.register(Accomodation, AccomodationAdmin)
+admin.site.register(Amenities)
