6460e16f5f6cd3a67ac946adcff1972ae1b0683b
Alvin Awuor <alvinawuor8@gmail.com>
2021-06-10 11:38:06 +0300

    Add contact form.

--- gethouse/forms.py
+++ gethouse/forms.py
@@ -10,3 +10,8 @@ class AccomodationForm(forms.ModelForm):
 class SubscriptionForm(forms.Form):
     name = forms.CharField(label='First Name', max_length=30)
     email = forms.EmailField(label='Email')
+
+
+class ContactForm(forms.Form):
+    email = forms.EmailField(label='Email')
+    context = forms.CharField(widget=forms.Textarea)
